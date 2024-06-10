f48b79e6858bf777c42497b0a0704dd37fcb6b45
jessicazambranocg <jessi.zambrano18@gmail.com>
2024-06-10 13:35:53 -0400

    feat: finish excersice repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function(string, num) {
-
-};
+const repeatString = function (stringWord, num) {
+    let string = "";
+    for (let i = 0; i < num; i++) {
+        if(num < 0){
+            return "NOTSTRING";
+            string += stringWord;
+        }else{
+            return string;
+        }
+        }  
+    };
+    
 
 // Do not edit below this line
 module.exports = repeatString;
