abec87c4b8f4efa457c11f662659cd78a8bff519
jessicazambranocg <jessi.zambrano18@gmail.com>
2024-06-17 13:48:59 -0400

    feat: finish excersice removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numAndWord) {
+  const newArray=[];
+  array.forEach((element) => {
+    if(!numAndWord.includes(element)){
+    newArray.push(element);
+  }
+});
+  return newArray;
 };
 
 // Do not edit below this line
