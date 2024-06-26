d9d61efe7914dfc5657cffc5e86466bc1be0acf8
jessicazambranocg <jessi.zambrano18@gmail.com>
2024-06-26 13:39:50 -0400

    feat: finish excersice sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(numMin, numMax) {
+  if (!Number.isInteger(numMin) || !Number.isInteger(numMax)) return "ERROR";
+  if (numMin < 0 || numMax < 0) return "ERROR";
+  if (numMin > numMax) {
+    const temp = numMin;
+    numMin = numMax;
+    numMax = temp;
+  }
 
+
+let sum = 0;
+  for (let i = numMin; i <= numMax; i++) {
+    sum += i;
+  }
+  return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
