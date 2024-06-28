a388c0dc3baa4d39b3ba4894707ae0e403374269
jessicazambranocg <jessi.zambrano18@gmail.com>
2024-06-28 12:29:50 -0400

    feat:finish excercise leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  let checkYear = (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) ? 1 : 0;
+  if (! checkYear )
+      return false;
+  else
+      return true;
 };
 
 // Do not edit below this line
